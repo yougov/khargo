fc5bdf3302e30d6c72b96814ead4dd33430692ee
Francesco Boscarino <francesco@bizmate.it>
2019-11-04 08:24:38 +0100

    Build

--- build.sh
+++ build.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 
-VERSION=$(git describe --always --long --dirty)
+VERSION=$(git describe --dirty)
 
 GOOS="linux" GOARCH="amd64" go build -ldflags "-s -w -X main.version=${VERSION}"
-docker build -t registry.bizmate.it/view/view-probe:linux .
+docker build -t registry.bizmate.it/view/view-gridfs:${VERSION:-linux} .
 
-GOOS="linux" GOARCH="arm" go build -ldflags "-s -w -X main.version=${VERSION}"
-docker build -t registry.bizmate.it/view/view-probe:arm .
-
-GOOS="darwin" GOARCH="amd64" go build -ldflags "-s -w -X main.version=${VERSION}"
-#docker build -t registry.bizmate.it/view/view-probe:darwin .
-
-docker push registry.bizmate.it/view/view-probe
+docker push registry.bizmate.it/view/view-gridfs
